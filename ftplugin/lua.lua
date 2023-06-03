98c0d272a8ca1bb9a9453a6be02d0cbb81a2e0fa
Arturito <arthur.brouart@gmail.com>
2023-06-03 22:26:12 +0200

    [lua] set global variable 'vim'

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,6 +1,13 @@
-require 'lspconfig'.lua_ls.setup {}
-
--- Indentation
 vim.cmd([[set shiftwidth=2 smarttab]])
 vim.cmd([[set expandtab]])
 vim.cmd([[set tabstop=8 softtabstop=0]])
+
+require('lspconfig').lua_ls.setup{
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}
