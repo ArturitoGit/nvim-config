02c7cbbb5ce8b16f0f0db2fa7f51cccd516677e0
= <=>
2023-06-10 20:36:57 +0200

    LSP/SQL: Move config into lsp_config file

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,3 +1,4 @@
+-- LUA
 require('lspconfig').lua_ls.setup {
   capablities = require('cmp_nvim_lsp').default_capabilities(),
   settings = {
@@ -8,6 +9,23 @@ require('lspconfig').lua_ls.setup {
           'describe', 'it',     -- Busted test
         }
       }
+    }
+  }
+}
+
+-- SQL
+require('lspconfig').sqlls.setup {
+  root_dir = function() return vim.env.HOME.."/.config/sql-language-server/" end,
+  connections = {
+    {
+      name= "local",
+      adapter= "mysql",
+      host= "localhost",
+      port= 3306,
+      user= "root",
+      password= "root",
+      database= "test",
+      projectPaths= { "/tmp" },
     }
   }
 }
