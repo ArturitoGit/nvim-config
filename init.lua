bc104ddec6dbf175d7f2b00e0839f564ebba2792
= <=>
2023-08-09 22:30:47 +0200

    Global/Remap/Indent: Keep selection in visual mode

--- init.lua
+++ init.lua
@@ -56,3 +56,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
+
+-- Easy indenting
+vim.keymap.set('v', '<lt>', '<gv')
+vim.keymap.set('v', '>', '>gv')
