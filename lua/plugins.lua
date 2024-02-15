037c3bae0360f62cf55442f1ab83bb5438bacea9
= <=>
2024-02-16 00:28:37 +0100

    Telescope: use latest

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,7 +12,7 @@ return require('packer').startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use {'nvim-telescope/telescope-ui-select.nvim' }
