dbc2169595440ebf502ff24b109635396a5a76db
Arthur Brouart <arthur.brouart@lyra-network.com>
2024-02-19 09:00:14 +0100

    Wildignore: Ignore target folders

--- init.lua
+++ init.lua
@@ -47,7 +47,7 @@ vim.cmd('packadd cfilter')
 -- Enable :find
 vim.o.path = vim.o.path..'**'
 vim.o.wildmenu = true
-vim.o.wildignore = '**/dist/**,**/node_modules/**,**/.git/**,**/public/**,**/*.class'
+vim.o.wildignore = '**/target/**,**/dist/**,**/node_modules/**,**/.git/**,**/public/**,**/*.class'
 
 -- Keep visual selection after indenting
 vim.keymap.set('v', '<lt>', '<gv')
