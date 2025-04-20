48c494224de5f269e0df9314502351355c28fc35
= <=>
2025-04-20 19:09:32 +0200

    fix(Test): Use PlenaryBustedFile

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,2 +1,15 @@
+local function create_exit_keymap(key)
+  vim.keymap.set({ 'n', 't' }, key, '<Cmd>quit<CR>', { silent = true, buffer = true })
+end
+
 -- Test current file
-vim.api.nvim_create_user_command('Tn', [[:vsplit | term nvim --headless -c 'PlenaryBustedFile %'<CR>]], {})
+vim.api.nvim_create_user_command('Test', function()
+
+  -- Run tests
+  vim.cmd('PlenaryBustedFile %')
+
+  -- Quit window on q
+  create_exit_keymap('q')
+  create_exit_keymap('<Esc>')
+
+end, {})
